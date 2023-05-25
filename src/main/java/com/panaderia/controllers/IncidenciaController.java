e5d4cf790f06092c3d2b995ca4f316bd11d19bb3
NoeliaxRodriguez <100428957@alumnos.uc3m.es>
2023-05-25 20:37:14 +0200

    actualizacion funcion get by mail

--- src/main/java/com/panaderia/controllers/IncidenciaController.java
+++ src/main/java/com/panaderia/controllers/IncidenciaController.java
@@ -1,2 +1,130 @@
-package com.panaderia.controllers;public class IncidenciaController {
+package com.panaderia.controllers;
+
+import com.panaderia.domains.Incidencia;
+import com.panaderia.repositories.IncidenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class IncidenciaController {
+    // CRUD de incidencias
+    @Autowired
+    IncidenciaRepository incidenciaRepository;
+
+    // GET - Extraer info de bbdd (listar todos las incidencias)
+    @RequestMapping(value = "/incidencias", method = RequestMethod.GET)
+    public @ResponseBody
+    ResponseEntity<List<Incidencia>> getAllProducts() {
+        List<Incidencia> incidenciaList = null;
+        try {
+            incidenciaList = incidenciaRepository.findAll();
+            if (incidenciaList == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        }catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(incidenciaList, HttpStatus.OK);
+    }
+
+    // GET (by id) - Extraer info de bbdd (listar una incidencia por su id)
+    @RequestMapping(value = "/incidencia/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    ResponseEntity<Incidencia> getIncidenciaId(@PathVariable String id) {
+        Incidencia fromDbInc = null;
+        try {
+            fromDbInc = incidenciaRepository.findById(id).orElse(null);
+            if(fromDbInc == null){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(fromDbInc, HttpStatus.OK);
+    }
+
+    // GET (by mail) - Extraer info de bbdd (listar incidencias por su mail)
+    @RequestMapping(value = "/incidencia/mail/{mail}", method = RequestMethod.GET)
+    public @ResponseBody
+    ResponseEntity<List<Incidencia>> getIncidenciaMail(@PathVariable String mail) {
+        List<Incidencia> incidenciaList = null;
+        try {
+            incidenciaList = incidenciaRepository.findIncidenciaByMail(mail);
+            if (incidenciaList == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        }catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(incidenciaList, HttpStatus.OK);
+    }
+
+    //POST
+    @PostMapping(value = "/incidencias", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<Incidencia> insertUser(@RequestBody Incidencia i) {
+        try {
+            if(i.equals(null)){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            incidenciaRepository.save(i);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(i, HttpStatus.OK);
+    }
+
+    // PUT - Actualización de info en BBDD
+    @RequestMapping(value = "/incidencia/{id}", method = RequestMethod.PUT)
+    public @ResponseBody
+    ResponseEntity<Incidencia> updateUser(@PathVariable String id, @RequestBody Incidencia toUpdate) {
+        Incidencia actInc = null;
+        try {
+            if((id == null) || (toUpdate == null)) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            // Buscamos por nombre de usuario y en caso de no encontrar nada, obtenemos null
+            Incidencia fromDBInc = incidenciaRepository.findById(id).orElse(null);
+
+            if (fromDBInc != null) {
+                incidenciaRepository.save(toUpdate);
+                actInc = toUpdate;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(actInc, HttpStatus.OK);
+    }
+
+    // DELETE - Eliminación de info en BBDD
+    @RequestMapping(value ="/incidencias/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody
+    ResponseEntity<Incidencia> deleteOne(@PathVariable String id) {
+        Incidencia toDeleteInc = null;
+        try {
+            if (id == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            // Buscamos por nombre de usuario y en caso de no encontrar nada, obtenemos null
+            toDeleteInc = incidenciaRepository.findById(id).orElse(null);
+            incidenciaRepository.delete(toDeleteInc);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(toDeleteInc, HttpStatus.OK);
+    }
 }
--- src/main/java/com/panaderia/repositories/IncidenciaRepository.java
+++ src/main/java/com/panaderia/repositories/IncidenciaRepository.java
@@ -1,2 +1,14 @@
-package com.panaderia.repositories;public interface IncidenciaRepository {
+package com.panaderia.repositories;
+
+import com.panaderia.domains.Incidencia;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Locale;
+
+public interface IncidenciaRepository extends CrudRepository <Incidencia, String>{
+    List<Incidencia> findAll();
+    List<Incidencia> findIncidenciaByMail(String mail);
 }
+
+
